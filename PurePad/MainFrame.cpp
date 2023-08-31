2b66fba4209b07b00f88f06e259a86fa2e4abf43
anupyldd <139476397+anupyldd@users.noreply.github.com>
2023-08-31 21:04:50 +0300

    general shape and placeholder icons

--- PurePad/MainFrame.cpp
+++ PurePad/MainFrame.cpp
@@ -10,8 +10,35 @@ MainFrame::MainFrame(const wxString& title, const wxSize& size,	long style)
 	genSizer = new wxBoxSizer(wxVERTICAL);
 
 	genNotebook = new wxNotebook(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, 0);
+	defaultPage = new wxPanel(genNotebook, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL);
+	genNotebook->AddPage(defaultPage, wxT("Page 1"), false);
 
 	genSizer->Add(genNotebook, 1, wxEXPAND | wxALL, 0);
+
+	wxBoxSizer* bottomPanelSizer;
+	bottomPanelSizer = new wxBoxSizer(wxHORIZONTAL);
+
+	addBtn = new wxBitmapButton(this, wxID_ANY, wxNullBitmap, wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW | wxBORDER_NONE);
+
+	//addBtn->SetBitmap(wxBitmap(wxT("Icons/phAdd.png"), wxBITMAP_TYPE_ANY));
+	bottomPanelSizer->Add(addBtn, 0, wxALL, 0);
+
+	removeBtn = new wxBitmapButton(this, wxID_ANY, wxNullBitmap, wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW | wxBORDER_NONE);
+
+	//removeBtn->SetBitmap(wxBitmap(wxT("Icons/phRemove.png"), wxBITMAP_TYPE_ANY));
+	bottomPanelSizer->Add(removeBtn, 0, wxLEFT, 5);
+
+	bottomFillerLabel = new wxStaticText(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0);
+	bottomFillerLabel->Wrap(-1);
+	bottomPanelSizer->Add(bottomFillerLabel, 1, wxALL | wxEXPAND, 5);
+
+	themeBtn = new wxBitmapButton(this, wxID_ANY, wxNullBitmap, wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW | wxBORDER_NONE);
+
+	//themeBtn->SetBitmap(wxBitmap(wxT("Icons/phDark.png"), wxBITMAP_TYPE_ANY));
+	bottomPanelSizer->Add(themeBtn, 0, wxALL, 0);
+
+
+	genSizer->Add(bottomPanelSizer, 0, wxEXPAND, 0);
 
 
 	this->SetSizer(genSizer);
--- PurePad/MainFrame.h
+++ PurePad/MainFrame.h
@@ -6,9 +6,13 @@ class MainFrame : public wxFrame
 {
 private:
 	wxNotebook* genNotebook;
+	wxPanel* defaultPage;
+	wxBitmapButton* addBtn;
+	wxBitmapButton* removeBtn;
+	wxStaticText* bottomFillerLabel;
+	wxBitmapButton* themeBtn;
 
 public:
 	MainFrame(const wxString& title = "PurePad", const wxSize& size = wxSize(500, 300),
 		long style = wxFRAME_TOOL_WINDOW | wxSTAY_ON_TOP | wxBORDER_NONE | wxTAB_TRAVERSAL);
 };
-
